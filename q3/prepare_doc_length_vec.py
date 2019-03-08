29b2fa696b4242b4fda3527306019b56d9b3cbf0
rajaupadhyay <upadhyayaraja@gmail.com>
2019-03-08 12:34:39 +0000

    laplace smoothing

---
+++ q3/prepare_doc_length_vec.py
@@ -0,0 +1,47 @@
+import pickle
+import numpy as np
+import redis
+from scipy.sparse import coo_matrix
+
+def pickle_object(obj, filename):
+    with open(filename+'.pickle', 'wb') as handle:
+        pickle.dump(obj, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+conn = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
+print(conn.keys())
+print(conn.hget('vocab_dictionary', 'won'))
+
+# dtm_mat = open('doc_term_matrix.pickle', 'rb')
+# test_dtm = pickle.load(dtm_mat)
+# dtm_mat.close()
+#
+# # print(test_dtm.getrow(42748))
+# # 1963_Estonian_SSR_Football_Championship  42748
+#
+# row_sums = test_dtm.sum(1)
+# test_dtm = None
+# print(row_sums.shape)
+#
+#
+# row_sums = np.array(row_sums).reshape(-1,).tolist()
+# print(len(row_sums))
+#
+# print(row_sums[42748])
+# pickle_object(row_sums, 'doc_length_vector')
+
+# print(type(row_sums))
+# row_sums = row_sums.flatten()
+# print(row_sums.shape)
+#
+# row_sums = np.squeeze(row_sums)
+# print(row_sums.shape)
+#
+# print(row_sums[42748])
+
+
+dl_v_f = open('doc_length_vector.pickle', 'rb')
+dl_v = pickle.load(dl_v_f)
+dl_v_f.close()
+
+print(dl_v[42748])
--- q3/query_lh_model_laplace_smoothing.py
+++ q3/query_lh_model_laplace_smoothing.py
@@ -56,18 +56,33 @@ Dict2['docName'] *= ((1/docLength+VOCABSIZE)^(missingQueryTerms))
 
 The following items are required:
 Inverted index with normalised frequency: ividx_with_freq.sqlite
-A vector containing the length of each document: CREATE
+A vector containing the length of each document: doc_length_vector.pickle
 
 '''
+
+doc_length_vec_f = open('doc_length_vector.pickle', 'rb')
+doc_length_vec = pickle.load(doc_length_vec_f)
+doc_length_vec_f.close()
+
+VOCABSIZE = 3922275
+
 
 def retrieve_top_five_docs(tokens):
     tokens = [tkn for tkn in tokens if conn.hget('vocab_dictionary', tkn)]
 
     tkn_indices = [int(conn.hget('vocab_dictionary', tkn)) for tkn in tokens]
 
+    length_of_claim = len(tokens)
+
     # Key: Doc number/ID, value: cosine similarity
     # Set defaultdict to start with 1 (because we're multplying the probs)
     result_dictionary = defaultdict(lambda: 1.0)
+
+    # Maintain this dict to see how many query terms have been added per doc
+    # If a doc does not have a particular query then the final value must be multiplied
+    # by ((1/docLength+VOCABSIZE)^(missingQueryTerms)) to account for the missing values
+    # This dict will help us calculate missingQueryTerms
+    words_added = defaultdict(int)
 
     with SqliteDict('ividx_with_freq.sqlite', decode=decompress_set) as ividx_dct:
         for tkn_idx in range(len(tokens)):
@@ -76,9 +91,92 @@ def retrieve_top_five_docs(tokens):
             docs_from_ividx = ividx_dct['ividx_{}'.format(tkn)]
 
             for doc_tuple in docs_from_ividx:
-                result_dictionary[doc_tuple[0]] *= doc_tuple[1]
+                curr_doc_length = doc_length_vec[int(doc_tuple[0])]
+                # Apply Laplace smoothing
+                # Add 1 and divide by the (document length + Vocab size)
+                result_dictionary[doc_tuple[0]] *= ((doc_tuple[1] * curr_doc_length) + 1)/(curr_doc_length + VOCABSIZE)
+                words_added[doc_tuple[0]] += 1
+
+    formatted_result_dictionary = {}
+
+    for k, v in result_dictionary.items():
+        new_value = v
+        if words_added[k] != length_of_claim:
+            missingQueryTerms = length_of_claim - words_added[k]
+            assert(missingQueryTerms>=1)
+            curr_doc_length = doc_length_vec[int(k)]
+            new_value = v * ((1/curr_doc_length+VOCABSIZE)**(missingQueryTerms))
+
+        formatted_result_dictionary[k] = new_value
 
 
-    top_5_document_indices = sorted(result_dictionary, key=result_dictionary.get, reverse=True)[:5]
+    top_5_document_indices = sorted(formatted_result_dictionary, key=formatted_result_dictionary.get, reverse=True)[:5]
 
     return top_5_document_indices
+
+
+
+
+'''
+Iterate over the training file containing the claims
+Retrieve top 5 docs for each claim
+'''
+translator = str.maketrans('', '', string.punctuation)
+
+stop_words = set(stopwords.words('english'))
+stop_words = stop_words.union(['-lrb-', '-rrb-'])
+
+porter_stemmer = PorterStemmer()
+
+def tokenise_claim(claim):
+
+    tokens = word_tokenize(claim)
+
+    # Lowercase
+    tokens = list(map(lambda x: x.lower(), tokens))
+    # Remove stopwords
+    tokens = list(filter(lambda l_ph: l_ph not in stop_words, tokens))
+    # Remove punctuation and stem
+    tokens = [porter_stemmer.stem((val.translate(translator))) for val in tokens]
+    # tokens = list(map(lambda val: PorterStemmer().stem(val), tokens))
+
+    # tokens = set(tokens)
+    if '' in tokens:
+        while '' in tokens:
+            tokens.remove('')
+
+    tokens = list(tokens)
+    return tokens
+
+
+
+def file_reader_generator(file_object):
+    while True:
+        data = file_object.readline()
+        if not data:
+            break
+        yield data
+
+
+total_itrs = 0
+start_time = time.time()
+with open('data/train.jsonl', 'r') as openfile:
+    for line in file_reader_generator(openfile):
+        if total_itrs == 10:
+            break
+
+        if total_itrs%50 == 0:
+            end_time = time.time()
+            total_time = end_time - start_time
+            print(total_itrs, total_time)
+            start_time = time.time()
+
+        total_itrs += 1
+
+        json_dict = json.loads(line)
+        curr_claim = json_dict['claim']
+        claim_id = json_dict['id']
+        tokenised_claim = tokenise_claim(curr_claim)
+
+        res = retrieve_top_five_docs(tokenised_claim)
+        print(claim_id, res)
